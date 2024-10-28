15d46fd7e3c881def85088db8c81f8624c2970da
FakhriadiRasyaad <punyafakhri1@gmail.com>
2024-10-28 10:01:58 +0700

    upnew

--- src/pages/profil/setting.tsx
+++ src/pages/profil/setting.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import supabase from '../../components/SupabaseClient';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const SettingsPage = () => {
   const [fullName, setFullName] = useState('');
@@ -61,7 +61,7 @@ const SettingsPage = () => {
 
     const { data: signedUrlData, error: signedUrlError } = await supabase.storage
       .from('avatars')
-      .createSignedUrl(uploadData.path, 60 * 60);
+      .createSignedUrl(uploadData.path, 60 * 60 * 24 * 365 * 10);
 
     if (signedUrlError) {
       console.error('Error creating signed URL:', signedUrlError);
@@ -121,7 +121,7 @@ const SettingsPage = () => {
         
         {/* Avatar */}
         {avatar && (
-          <Image
+          <img
             src={avatar} 
             alt="Avatar" 
             className="h-36 w-36 rounded-full mb-6 border-2 border-gray-300 shadow-md" 
